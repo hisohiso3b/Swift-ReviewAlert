ca086f11f2ccab3005233e13093ef6675767c1e5
hisohiso3b <hisohiso3b@gmail.com>
2015-07-23 15:50:05 +0900

    Initial Commit

---
+++ ReviewAlert/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReviewAlert
+//
+//  Created by hiso on 2015/07/23.
+//  Copyright (c) 2015年 祐輔 花田. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
