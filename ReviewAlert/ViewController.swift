0164a934101756d227b3a36ed766b23aaeaed0ac
hisohiso3b <hisohiso3b@gmail.com>
2015-07-23 15:55:07 +0900

    add review alart

--- ReviewAlert/ViewController.swift
+++ ReviewAlert/ViewController.swift
@@ -9,15 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let ud = NSUserDefaults.standardUserDefaults()
+    
+    override func viewDidAppear(animated: Bool) {
+        if !ud.boolForKey("reviewed") {
+            let alertController = UIAlertController(
+                title: "Congraturation!!!",
+                message: "Thankyou for playing!!\nCould you review this app?",
+                preferredStyle: .Alert)
+            
+            let reviewAction = UIAlertAction(title: "Review now", style: .Default) {
+                action in
+                let url = NSURL(string: "APP_STORE_URL")
+                UIApplication.sharedApplication().openURL(url!)
+                self.ud.setObject(true, forKey: "reviewed")
+            }
+            let yetAction = UIAlertAction(title: "not now", style: .Default) {
+                action in
+                self.ud.setObject(false, forKey: "reviewed")
+            }
+            let neverAction = UIAlertAction(title: "NEVER", style: .Cancel) {
+                action in
+                self.ud.setObject(true, forKey: "reviewed")
+            }
+            
+            alertController.addAction(reviewAction)
+            alertController.addAction(yetAction)
+            alertController.addAction(neverAction)
+            presentViewController(alertController, animated: true, completion: nil)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
